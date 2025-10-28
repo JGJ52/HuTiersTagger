8fafc64fe46dff33592e111ee82a6d2faacf552b
jgj52 <email@jgj52.hu>
2025-10-28 17:30:27 +0100

    trying to fix icons

--- src/client/java/hu/jgj52/hutierstagger/mixin/PlayerEntityMixin.java
+++ src/client/java/hu/jgj52/hutierstagger/mixin/PlayerEntityMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -53,6 +54,7 @@ public abstract class PlayerEntityMixin {
                 case "shieldlessuhc" -> "";
                 default -> "";
             };
+            Text iconT = Text.literal(icon).setStyle(Style.EMPTY.withFont(Identifier.of("hutierstagger:default")));
             if (retired) {
                 if (prefix.startsWith("HT")) {
                     color = "651d6e";
@@ -61,13 +63,13 @@ public abstract class PlayerEntityMixin {
                 }
                 prefix = "R" + prefix;
             }
-            cir.setReturnValue(Text.literal("").append(Text.literal(icon + " ")).append(Text.literal(prefix).setStyle(Style.EMPTY.withColor(TextColor.fromRgb(Integer.parseInt(color, 16))))).append(Text.literal(" §8| §r")).append(Text.literal(playerName)));
+            cir.setReturnValue(Text.literal("").append(iconT).append(Text.literal(prefix).setStyle(Style.EMPTY.withColor(TextColor.fromRgb(Integer.parseInt(color, 16))))).append(Text.literal(" §8| §r")).append(Text.literal(playerName)));
         }
     }
 
     private static String getPlayerName(PlayerEntity player) {
         try {
-            return player.getGameProfile().name();
+            return player.getGameProfile().getName();
         } catch (NoSuchMethodError e) {
             try {
                 return (String) player.getGameProfile()
